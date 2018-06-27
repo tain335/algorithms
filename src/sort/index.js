138e4833f788d4341b17656f38cfb1932b569e7d
tain335 <649397393@qq.com>
2018-06-28 00:02:51 +0800

    update sort

--- src/sort/index.js
+++ src/sort/index.js
@@ -29,10 +29,73 @@ function quickSort(arr) {
     return arr;
 }
 
+function _part2(arr, start, end) {
+    if(start >= end) return;
+    let i = start;
+    let j = end;
+    let midVal = arr[start];
+    while(true) {
+        while(i < j && arr[j] < midVal) j--;
+        if(i < j) {
+            arr[i] = arr[j];
+        }
+        while(i < j && arr[i] >= midVal) i++;
+        if(i < j) {
+            arr[j] = arr[i];
+        } else {
+            break;
+        }
+    }
+    arr[i] = midVal;
+    _part2(arr, start, i - 1);
+    _part2(arr, i + 1, end);
+}
 
-function mergeSort() {
+function quickSort2(arr) {
+    if(arr.length < 2) return arr;
+    _part2(arr, 0, arr.length - 1); 
+    return arr;
+}
 
+function _mergeArr(arr, start, mid, end) {
+    let i = start;
+    let j = mid + 1;
+    let faker = [];
+    for(let k = start; k <= end; k++) {
+        faker[k] = arr[k];
+    }
+    while(i <= mid && j <= end) {
+        if(faker[j] >= faker[i]) {
+            arr[start++] = faker[j];
+            j++; 
+        } else {
+            arr[start++] = faker[i];
+            i++;
+        }
+    }
+    while(i <= mid) {
+        arr[start++] = faker[i++];
+    }
+    while(j <= end) {
+        arr[start++] = faker[j++];
+    }
+    return arr;
+}
+
+function _mergeSort(arr, start, end) {
+    if(start >= end) return;
+    let mid = Math.floor((start + end) / 2);
+    _mergeSort(arr, start, mid);
+    _mergeSort(arr, mid + 1, end);
+    _mergeArr(arr, start, mid, end);
+}
+
+function mergeSort(arr) {
+    if(arr.length < 2) return arr;
+    _mergeSort(arr, 0, arr.length - 1);
+    return arr;
 }
 
 exports.quickSort = quickSort;
+exports.quickSort2 = quickSort2;
 exports.mergeSort = mergeSort;
--- src/sort/test.js
+++ src/sort/test.js
@@ -1,6 +1,6 @@
 let Sort = require('./index');
 
-describe("sort", () => {
+describe("quickSort", () => {
     test("quickSort should be equal", () => {
         expect(Sort.quickSort([])).toEqual([]);
     });
@@ -19,5 +19,49 @@ describe("sort", () => {
 
     test("quickSort should be equal", () => {
         expect(Sort.quickSort([1, 2, 3, 4, 5, 6])).toEqual([6, 5, 4, 3, 2, 1]);
+    });
+})
+
+describe("quickSort2", () => {
+    test("quickSort2 should be equal", () => {
+        expect(Sort.quickSort2([])).toEqual([]);
+    });
+
+    test("quickSort should be equal", () => {
+        expect(Sort.quickSort2([0])).toEqual([0]);
+    });
+
+    test("quickSort should be equal", () => {
+        expect(Sort.quickSort2([4, 5])).toEqual([5, 4]);
+    });
+
+    test("quickSort should be equal", () => {
+        expect(Sort.quickSort2([1, 0, 3, 7, 8])).toEqual([8, 7, 3, 1, 0]);
+    });
+
+    test("quickSort should be equal", () => {
+        expect(Sort.quickSort2([1, 2, 3, 4, 5, 6])).toEqual([6, 5, 4, 3, 2, 1]);
+    });
+});
+
+describe("mergeSort", () => {
+    test("mergeSort should be equal", () => {
+        expect(Sort.mergeSort([])).toEqual([]);
+    });
+
+    test("mergeSort should be equal", () => {
+        expect(Sort.mergeSort([0])).toEqual([0]);
+    });
+
+    test("mergeSort should be equal", () => {
+        expect(Sort.mergeSort([4, 5])).toEqual([5, 4]);
+    });
+
+    test("mergeSort should be equal", () => {
+        expect(Sort.mergeSort([1, 0, 3, 7, 8])).toEqual([8, 7, 3, 1, 0]);
+    });
+
+    test("mergeSort should be equal", () => {
+        expect(Sort.mergeSort([1, 2, 3, 4, 5, 6])).toEqual([6, 5, 4, 3, 2, 1]);
     });
 })
